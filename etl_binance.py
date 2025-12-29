04b149b858832d35b0b9124ba9976efc1d44dcc2
EuLucasSlva <lucasconsultoria2@gmail.com>
2025-12-29 19:51:55 -0300

    Fix: adicionando logs de erro e verificacao de secrets

--- etl_binance.py
+++ etl_binance.py
@@ -17,7 +17,9 @@ params = urllib.parse.quote_plus(
     f'DATABASE={DATABASE};'
     f'UID={USERNAME};'
     f'PWD={PASSWORD};'
+    f'Encrypt=yes;' # Adicione isto explicitamente
     f'TrustServerCertificate=yes;'
+    f'Connection Timeout=30;' # Aumentamos o tempo de espera
 )
 engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
 
